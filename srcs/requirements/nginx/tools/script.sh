3123b8e87568435e17f3aac382a45b253ffe3306
VatcheTavitian <vatchetavitian86@gmail.com>
2024-02-08 14:52:35 +0400

    Changed nginx variables

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTIFICATE -subj "/C=AM/L=YVN/O=42YVN/OU=42/CN=vtavitia.42.fr"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTIFICATE -subj $VALUE_
 
 echo "SSL CERTIFICATE GENERATED - default file configuration complete"
 
