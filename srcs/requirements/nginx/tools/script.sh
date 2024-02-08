f7d8d71ee8ebf2f7bf215f06a8a08738bd627160
vatchetavitian <vatchetavitian86@gmail.com>
2024-02-08 06:32:57 -0500

    updated cert path variable and mariadb root pass

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTIFICATE -subj $VALUE_
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTIFICATE_PATH -subj $VALUE_
 
 echo "SSL CERTIFICATE GENERATED - default file configuration complete"
 
